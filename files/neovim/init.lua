3ec682cf6a037fa458e8a956ccc9d3e396d39ce0
Egon Braun <egon@mundoalem.io>
2024-11-27 14:47:21 +0100

    Formats lua code

--- files/neovim/init.lua
+++ files/neovim/init.lua
@@ -162,7 +162,7 @@ require("lazy").setup({
         },
 
         spec = {
-            { "<leader>c", group = "[C]ode", mode = { "n", "x" } },
+            { "<leader>c", group = "[C]ode",     mode = { "n", "x" } },
             { "<leader>d", group = "[D]ocument" },
             { "<leader>r", group = "[R]ename" },
             { "<leader>s", group = "[S]earch" },
@@ -274,11 +274,11 @@ require("lazy").setup({
         -- stylua: ignore
 
         keys = {
-            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-            { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+            { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+            { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+            { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+            { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
         },
     },
 
@@ -312,7 +312,7 @@ require("lazy").setup({
             "WhoIsSethDaniel/mason-tool-installer.nvim",
 
             -- Useful status updates for LSP.
-            { "j-hui/fidget.nvim", opts = {} },
+            { "j-hui/fidget.nvim",       opts = {} },
 
             -- Allows extra capabilities provided by nvim-cmp
             "hrsh7th/cmp-nvim-lsp",
